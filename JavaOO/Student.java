60bc42aa27daf7f6fc04d651fbce491d85e97edd
Dooy94 <dooy94@gmail.com>
2017-04-04 17:00:17 +0900

    1번 문제
    시스템분석설계 프로그래밍 연습 
    1.다음을 만족하는 Student 클래스를 작성하시오.
    -String형의 학과와 정수형의 학번을 필드로 선언.
    -Student 클래스의 main()메소드에서 Student 객체를 생성하여 학과와 학번 필드에 적당한 값을 입력 후 출력.

---
+++ JavaOO/Student.java
@@ -0,0 +1,20 @@
+package systemprj;
+
+class Student {
+
+	String hakgwa;
+	int stdnum;
+
+	void print(){
+		System.out.println("학과: "+hakgwa);
+		System.out.println("학번: "+stdnum);
+	}
+
+	public static void main(String args[]){
+		Student std = new Student();
+		std.hakgwa="컴퓨터정보공학과";
+		std.stdnum=20130672;
+		std.print();
+	}
+}
+
