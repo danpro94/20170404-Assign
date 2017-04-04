cff110fa2447ddbb774b8dee1f945f4ec9d0e04a
Dooy94 <dooy94@gmail.com>
2017-04-04 18:07:32 +0900

    3번 문제

---
+++ JavaOO/Circle.java
@@ -0,0 +1,16 @@
+package systemprj;
+
+public class Circle {
+	public double radius;
+	public static double PI=3.141592;
+
+	
+	public Circle(double radius) {
+		this.radius=radius;
+	}
+	
+	public double getArea(){
+		return radius * radius * PI;
+	}
+}
+
